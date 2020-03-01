c7b19a3671e83f68bcd3aa968213102b3989ceae
Parth Aggarwal <frazile.com@gmail.com>
2020-03-01 14:42:03 +0530

    userRepository added

--- lib/providers/userRepository.dart
+++ lib/providers/userRepository.dart
@@ -1,2 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
 
+class UserRepository with ChangeNotifier {
+  FirebaseAuth _auth;
+  FirebaseUser _user;
+  GoogleSignIn _googleSignIn;
+  Status _status = Status.Uninitialized;
+  bool loginSkipped = false;
+  String error = '';
+
+  UserRepository.instance()
+      : _auth = FirebaseAuth.instance,
+        _googleSignIn = GoogleSignIn() {
+    _auth.onAuthStateChanged.listen(_onAuthStateChanged);
+    _checkIsLoginSkipped();
+  }
+
+  Status get status => _status;
+  FirebaseUser get user => _user;
+
+  void _checkIsLoginSkipped() async {
+    // SharedPreferences prefs = await SharedPreferences.getInstance();
+    // loginSkipped = prefs.getBool('login_skipped') ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _onAuthStateChanged(FirebaseUser firebaseUser) async {
+    _user = firebaseUser;
+    if (firebaseUser == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _status = Status.Authenticated;
+    }
+    notifyListeners();
+  }
+}
