4c372884e4feb76424118cbea1b3353b558ad6b0
Parth Aggarwal <frazile.com@gmail.com>
2020-03-01 14:09:25 +0530

    Google Services added & Firebase packages added

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Firebase Flutter
 
 Firebase Flutter is a app to setup Firebase in flutter.
+
+## Packages
+
+1. firebase: ^7.2.1
+2. cupertino_icons: ^0.1.2
+3. firebase_auth: ^0.15.5+2
+4. firebase_crashlytics: ^0.1.3
+5. connectivity: ^0.4.8+1
+6. cached_network_image: ^2.0.0
+7. firebase_performance: ^0.3.1+5
+8. cloud_firestore: ^0.13.4
+9. firebase_analytics: ^5.0.11
+10. google_sign_in: ^4.1.4
+11. provider: ^4.0.4
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebaseflutter/pages/config/configPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -12,6 +13,15 @@ void main() {
   //* Forcing only portrait orientation
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(ConfigPage());
 }
