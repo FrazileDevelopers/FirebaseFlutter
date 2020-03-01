a79436a28e6de78fc1b44a7e3fc6e2366c526b30
Parth Aggarwal <frazile.com@gmail.com>
2020-03-01 12:45:40 +0530

    Code Updated

---
+++ lib/pages/config/configPage.dart
@@ -0,0 +1,16 @@
+import 'package:firebaseflutter/pages/home/homePage.dart';
+import 'package:flutter/material.dart';
+
+class ConfigPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
